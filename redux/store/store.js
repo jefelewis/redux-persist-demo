e844837218b70e7848a882d2d3c91390f2e74a8a
Jeff Lewis <jeffylew77@gmail.com>
2019-10-30 17:34:27 -0700

    [Fix] Linting

--- redux/store/store.js
+++ redux/store/store.js
@@ -1,5 +1,4 @@
 // Imports: Dependencies
-// import { AsyncStorage } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
